536e3766dc3ebe1fc2bad9b328d69511c8621fb4
galeks <galeks@bigmir.net>
2017-03-24 14:05:16 +0200

    7 lessons massive

--- index.php
+++ index.php
@@ -6,12 +6,24 @@
 </head>
 <body>
 <?php
-$my_var = 'Hello, Word';
 
-   echo $my_var;
+$fruits = array(5,10,3);
+
+$var = 'Something';
+$arr = array(5,10,3, 'some string',false, 2.54, $var);
+
+$apples_amount = $fruits[0];
+$bananas_amount = $fruits[1];
+$oranges_amount = $fruits[2];
+
+$fruits[2]=24; // no we get array of 5, 10, 24
+
+echo '<br>';
+
+$numbers = array(22 => 'twenty two', '100' => 'one hundred','45'=>'forty five');
+
+echo $numbers[100]; // we get 'one hundred'
 
    ?>
-
-    <p>Another paragraph</p>
 </body>
 </html>
